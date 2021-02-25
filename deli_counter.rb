c43c1b146094ab50e87c9eeeb5630aa09ff0647a
Jewel Carrier-Davis <jujicb@gmail.com>
2021-02-25 11:41:58 -0500

    commit tree 8426be05eaa44ec1c646b2d625ca3a2cb251941a

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+array = []
+
+def take_a_number(array, name)
+
+    new_array = array << name
+    puts "Welcome, #{name}. You are number #{new_array.length} in line."
+end
+
+
+def line(array = array)
+    if array.length === 0
+        puts "The line is currently empty."
+    else
+        array.map.with_index do |person, index|
+            array[index] = "#{index + 1}. #{person}"
+        end
+        puts "The line is currently: " + array.join(" ")
+    end
+end
+
+def now_serving(array)
+    if array.length === 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving " + array.shift + "."
+    end
+end
